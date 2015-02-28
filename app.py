3c33d0d371bca0295f9ed633e59bfbdc0ebb8a9f
lowicz <mc.lowicz@gmail.com>
2015-02-28 13:52:14 +0100

    Get keywords without polish chars, and get page content

--- app.py
+++ app.py
@@ -1,25 +1,35 @@
 from flask import Flask, render_template, request
 from bs4 import BeautifulSoup
-import urllib2
-import re
+import urllib2, re, trans
 
 app = Flask(__name__)
+
+def encoding_check(html_object):
+		try:
+			return html_object.headers['Content-Type'].split('charset=')[1]
+		except:
+			return 'utf8'	
+
+def encoding_page(html_text, enc):
+	return html_text.decode(enc).encode('trans')
+
+def get_keywords_list(html_text, enc):
+	content = encoding_page(html_text, enc)
+	keywordregex = re.compile("<meta name=\"Keywords\".*?content=\"([^\"]*)\"")
+	keywordlist = keywordregex.findall(content)
+	return str(keywordlist)[3:-2].split(',')
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
 	results = ''
-	keywordlist = []
+	html_object = ''
 	if request.method == 'POST':
-		url = request.form['url']
-		htmlobj = urllib2.urlopen(url)
-		try:
-			encoding=htmlobj.headers['Content-Type'].split('charset=')[1]
-		except:
-			encoding = 'utf8'
-		ucontent = unicode(htmlobj.read(), encoding)
-		keywordregex = re.compile("<meta name=\"Keywords\".*?content=\"([^\"]*)\"")
-		keywordlist = keywordregex.findall(ucontent)
-    	results =  keywordlist
+		html_object = urllib2.urlopen(request.form['url'])
+		enc = encoding_check(html_object)
+		html_text = html_object.read()
+    	keywords = get_keywords_list(html_text, enc)
+    	soup = BeautifulSoup(encoding_page(html_text, enc))
+    	results = soup.get_text()
 	return render_template("index.html", results=results)
 
 if __name__ == '__main__':
